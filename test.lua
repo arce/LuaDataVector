876caa3b6bf66730dae88ea50a7164a24fc1b1a0
Armando Arce <armando.arce@gmail.com>
2018-11-20 16:27:35 -0600

    Args changed

--- test.lua
+++ test.lua
@@ -5,11 +5,12 @@ local vect1 = vector("Test1")
 vect1[1] = 10
 vect1[2] = 20
 
-local vect2 = vector("Test2")
-
-vect2[1] = 100
-vect2[2] = 200
-
 printVector(vect1)
 
+local vect2 = vector("Test2",{1,2,3,4,5})
+
 printVector(vect2)
+
+local vect3 = vector{'A','B','C','D'}
+
+printVector(vect3)

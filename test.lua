1435a5a22d66dd5d4836b9065a2c095610cf74ae
Armando Arce <armando.arce@gmail.com>
2018-11-20 11:42:51 -0600

    oid included

--- test.lua
+++ test.lua
@@ -1,11 +1,11 @@
 require "dataVector"
 
-local vect1 = vector()
+local vect1 = vector("Test1")
 
 vect1[1] = 10
 vect1[2] = 20
 
-local vect2 = vector()
+local vect2 = vector("Test2")
 
 vect2[1] = 100
 vect2[2] = 200
